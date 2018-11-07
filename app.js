ce0195acd0b5866db72809748c922f6c02000a26
Gyozmo <binantguillaume@gmail.com>
2018-11-07 14:56:47 +0100

    layouts

--- app.js
+++ app.js
@@ -4,10 +4,29 @@ var app = express()
 app.set('view engine', 'pug')
  
 
+//SQL PART
+var connection = require('./env2').connection
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
 //Routing
 app.get('/', function (req, res) {
-  res.render('index.pug', {title: 'Hey', message:'my pug'})
+  connection.query(`select * from user where username = 'gyo'`, function (error, results) {
+    res.render('index.pug', {title: 'Alumnis', message: `hey ${results[0].username} Bienvenue dans la page d'accueil alumnis Simplon`});
+    console.log(results[0])
+  });
+  
+
 })
+
+
+
 
 app.get('/profile', function(req,res){
   res.render('profil.pug')
@@ -23,16 +42,7 @@ app.get('/mateprofile', function(req,res){
  
 
 
-//SQL PART
-var connection = require('./env2').connection
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
 
 
 app.listen(3000, function(){
